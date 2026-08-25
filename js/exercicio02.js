f1725a1fbc3f9d442761c4d7f7dd85c6fa91ebef
Davi <davisousa7oliveira@gmail.com>
2026-08-25 13:25:36 -0300

    feat: pratica variaveis e tipos de dados

---
+++ js/exercicio02.js
@@ -0,0 +1,22 @@
+const nome = "Davi de Sousa Oliveira";
+let idade = 24;
+const cidade = "São Roque";
+const matriculado = true;
+const nota = 8.5;
+
+console.log(nome);
+console.log(idade);
+console.log(cidade);
+console.log(matriculado);
+console.log(nota);
+
+console.log(typeof nome);
+console.log(typeof idade);
+console.log(typeof cidade);
+console.log(typeof matriculado);
+console.log(typeof nota);
+
+// Usei const nas variáveis nome, cidade, matriculado e nota
+// porque esses valores não precisam ser alterados durante o programa.
+//
+// Usei let na idade porque esse valor pode ser alterado com o tempo.
