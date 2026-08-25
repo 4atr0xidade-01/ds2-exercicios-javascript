d5a25b9d7f842e8cd5614d97d30eb9f8785cc41f
Davi <davisousa7oliveira@gmail.com>
2026-08-25 13:46:50 -0300

    feat: coleta e apresenta dados do aluno

---
+++ js/exercicio03.js
@@ -0,0 +1,11 @@
+
+const nome = prompt("Qual é o seu nome?");
+let idade = prompt("Qual é a sua idade?");
+const curso = prompt("Qual é o seu curso?");
+const cidade = prompt("Qual é a sua cidade?");
+
+// Solução 1
+//console.log("Olá, meu nome é " + nome + ", tenho " + idade + " anos, moro em " + cidade + " e estou cursando " + curso + ".");
+
+// Solução 2
+console.log(`Olá, meu nome é ${nome}, tenho ${idade} anos, moro em ${cidade} e estou cursando ${curso}.`);
